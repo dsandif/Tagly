31a1ea5d48ad8a3316a5d9f469e6200a4cc1251c
Darien Sandifer <dariens@duck.com>
2023-01-01 23:19:21 -0600

    initial commit

--- Tagly/ContentView.swift
+++ Tagly/ContentView.swift
@@ -6,16 +6,162 @@
 //
 
 import SwiftUI
+import Haptica
+import AVKit
+
+enum Mode {
+    case ahead, right
+}
 
 struct ContentView: View {
+    @State var audioPlayer: AVAudioPlayer!
+    @State var mode = Mode.ahead
+    let timer = Timer.publish(every: 3, on: .main, in: .common).autoconnect()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            ZStack {
+                VStack {
+                    Circle()
+                        .fill(mode == .ahead ? Color.pink : .green)
+                        .frame(width: 16, height: 16)
+                        .offset(y: mode == .ahead ? -4 : -8)
+                        .scaleEffect(mode == .ahead ? 2 : 1)
+                    
+                    Spacer()
+                }
+                
+                Circle()
+                    .trim(from: 0.025, to: mode == .ahead ? 0.0 : 0.225)
+                    .rotation(Angle(degrees: -90))
+                    .stroke(Color.gray, style: StrokeStyle(lineWidth: 16, lineCap: .round))
+                    .opacity(mode == .ahead ? 0 : 1)
+            }
+            .frame(width: 312, height: 312)
+            
+            ZStack {
+                
+                HStack {
+                    Spacer()
+                    Circle()
+                        .frame(width: 16, height: 16)
+                        .offset(x: 8)
+                        .opacity(mode == .ahead ? 0 : 1)
+                }
+            }
+            .frame(width: 312, height: 312)
+            .rotationEffect(Angle(degrees: mode == .ahead ? -90 : 0))
+            
+            Image(systemName: "arrow.up")
+                .font(.system(size: 192, weight: .bold))
+                .rotationEffect(Angle(degrees: mode == .ahead ? 0 : 90))
+            
+            VStack {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("FINDING")
+                            .font(.footnote)
+                            .fontWeight(.medium)
+                            .foregroundColor(.secondary)
+                        
+                        Text("Darien's Keys")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                    }
+                    
+                    Spacer()
+                }
+                .padding(.top)
+                
+                Spacer()
+                
+                VStack {
+                    HStack {
+                        Text("20")
+                        Text("ft")
+                            .foregroundColor(.secondary)
+                        
+                        Spacer()
+                    }
+                    
+                    HStack {
+                        if mode == .right {
+                            Text("to your")
+                            Text("right")
+                                .foregroundColor(.secondary)
+                        } else {
+                            Text("ahead")
+                        }
+                        
+                        Spacer()
+                    }
+                }
+                .font(.system(size: 48, design: .rounded))
+                .padding(.bottom)
+                
+                HStack {
+                    Button(action: { }) {
+                        Image(systemName: "xmark")
+                            .font(.system(size: 24, weight: .semibold))
+                            .padding(20)
+                            .background(
+                                Circle()
+                                    .fill(.gray)
+                            )
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    
+                    Spacer()
+                    
+                    Button(action: { }) {
+                        Image(systemName: "speaker.wave.2.fill")
+                            .font(.system(size: 24, weight: .semibold))
+                            .padding(20)
+                            .background(
+                                Circle()
+                                    .fill(.gray)
+                            )
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                }
+                .padding(.horizontal)
+            }
+            .padding()
         }
-        .padding()
+        .onReceive(timer) { time in
+            toggleMode()
+        }
+        .background((mode == .ahead ? Color.green : Color.gray).edgesIgnoringSafeArea(.all))
+    }
+    
+    func toggleMode() {
+        if mode == .ahead {
+            withAnimation(.easeInOut(duration: 1)) {
+                mode = .right
+            }
+        } else {
+            withAnimation(.easeInOut(duration: 1)) {
+                mode = .ahead
+                Haptic.impact(.heavy).generate()
+                
+                #if !DEBUG
+                playSound()
+                #endif
+            }
+        }
+    }
+    
+    func playSound() {
+        // Load a local sound file
+        guard let soundFileURL = Bundle.main.path(
+            forResource: "sound_effect",
+            ofType: "mp3"
+        )else {
+            return
+        }
+
+        self.audioPlayer = try! AVAudioPlayer(contentsOf: URL(fileURLWithPath: soundFileURL))
+        self.audioPlayer.play()
     }
 }
 

340f2e4c34876110656ab3aeb5421312a4ee4662
Darien Sandifer <dariens@duck.com>
2023-01-01 22:30:57 -0600

    Initial Commit

---
+++ Tagly/TaglyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaglyApp.swift
+//  Tagly
+//
+//  Created by Darien Sandifer on 1/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TaglyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
